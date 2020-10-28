3a0138a57952a5f7e92784ec3787055322bc1f8a
Santiago LD <sldenazis@gmail.com>
2020-10-28 19:11:53 -0300

    deleted old option

--- alb.tf
+++ alb.tf
@@ -5,5 +5,4 @@ module "alb" {
   environment = var.environment
   vpc_id      = module.vpc.id
   subnets     = module.vpc.public_subnets
-  //ec2_target_instance_ids = [module.nginx.id, module.apache.id]
 }
