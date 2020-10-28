fba1396bdc4f7b8e669002ab29f30865fd4c4425
Santiago LD <sldenazis@gmail.com>
2020-10-28 19:11:42 -0300

    enable http traffic

--- modules/alb/security.tf
+++ modules/alb/security.tf
@@ -7,3 +7,29 @@ resource "aws_security_group" "security_group" {
     environment = var.environment
   }
 }
+
+resource "aws_security_group_rule" "http_ingress" {
+  type      = "ingress"
+  from_port = 80
+  to_port   = 80
+  protocol  = "tcp"
+
+  cidr_blocks = [
+    "0.0.0.0/0",
+  ]
+
+  security_group_id = aws_security_group.security_group.id
+}
+
+resource "aws_security_group_rule" "all_egress" {
+  type      = "egress"
+  from_port = 0
+  to_port   = 0
+  protocol  = "all"
+
+  cidr_blocks = [
+    "0.0.0.0/0",
+  ]
+
+  security_group_id = aws_security_group.security_group.id
+}
